4c211584573214e85f8545d4620578c0218979d7
cggos <cggos@outlook.com>
2019-08-20 14:41:43 +0800

    add yaml support and its unit test

---
+++ include/utils/config_io.h
@@ -0,0 +1,68 @@
+//
+// Created by cg on 8/20/19.
+//
+
+#ifndef MSCKF_VIO_MYNT_CONFIG_IO_H
+#define MSCKF_VIO_MYNT_CONFIG_IO_H
+
+#include <yaml-cpp/yaml.h>
+#include <Eigen/Core>
+
+namespace YAML {
+    // Vector2d
+    template<>
+    struct convert<Eigen::Vector2d> {
+        static bool decode(const Node& node, Eigen::Vector2d& rhs) {
+            if(!node.IsSequence() || node.size() != 2) {
+                return false;
+            }
+            rhs[0] = node[0].as<double>();
+            rhs[1] = node[1].as<double>();
+            return true;
+        }
+    };
+
+    // Vector4d
+    template<>
+    struct convert<Eigen::Vector4d> {
+        static Node encode(const Eigen::Vector4d& rhs) {
+            Node node;
+            node.push_back(rhs[0]);
+            node.push_back(rhs[1]);
+            node.push_back(rhs[2]);
+            node.push_back(rhs[3]);
+            return node;
+        }
+
+        static bool decode(const Node& node, Eigen::Vector4d& rhs) {
+            if(!node.IsSequence() || node.size() != 4) {
+                return false;
+            }
+            rhs[0] = node[0].as<double>();
+            rhs[1] = node[1].as<double>();
+            rhs[2] = node[2].as<double>();
+            rhs[3] = node[3].as<double>();
+            return true;
+        }
+    };
+
+    // Matrix4d
+    template<>
+    struct convert<Eigen::Matrix4d> {
+        static bool decode(const Node& node, Eigen::Matrix4d& rhs) {
+            if(!node.IsSequence() || node.size() != 16) {
+                return false;
+            }
+            for(int i=0; i<16; ++i)
+                rhs.data()[i] = node[i].as<double>();
+            return true;
+        }
+    };
+}
+
+class ConfigIO {
+public:
+    ConfigIO() {}
+};
+
+#endif //MSCKF_VIO_MYNT_CONFIG_IO_H
